4f46ee3e2d931dea913f384d0c3a9671fd02856d
KolevaMirena <mirenauni1818@abv.bg>
2024-08-15 09:56:33 +0300

    Insert authors and books

--- src/main/java/com/softuni/book_server/model/entity/Book.java
+++ src/main/java/com/softuni/book_server/model/entity/Book.java
@@ -1,18 +1,60 @@
 package com.softuni.book_server.model.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "book")
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;
+    private String title;
     private String isbn;
+    @ManyToOne
+    private Author author;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + author +
+                '}';
+    }
 }
---
+++ src/main/java/com/softuni/book_server/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.softuni.book_server.repository;
+
+
+import com.softuni.book_server.model.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
